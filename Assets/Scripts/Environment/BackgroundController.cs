702b2342ca7eddf266e7213402df006a5a4b0a38
ovsisg <ovsisg@gmail.com>
2025-09-22 22:20:57 +0100

    Reworked background and platform visuals, simplified parallax logic for smoother performance

---
+++ Assets/Scripts/Environment/BackgroundController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    private float initialPositionX;
+    private float backgroundLength;
+
+    [Header("Camera & Parallax")]
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float parallaxEffect = 0.5f;
+
+    private void Start()
+    {
+        initialPositionX = transform.position.x;
+        backgroundLength = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void Update()
+    {
+        float parallaxDistance = cameraTransform.position.x * parallaxEffect;
+        float cameraDistance = cameraTransform.position.x * (1 - parallaxEffect);
+
+        transform.position = new Vector3(initialPositionX + parallaxDistance, transform.position.y, transform.position.z);
+
+        if (cameraDistance > initialPositionX + backgroundLength)
+            initialPositionX += backgroundLength;
+        else if (cameraDistance < initialPositionX - backgroundLength)
+            initialPositionX -= backgroundLength;
+    }
+}
